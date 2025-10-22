1327a6f358c0eaee64566243422aba884db4f3c0
Cherie <cherrypighippo532@gmail.com>
2025-10-23 01:10:18 +0800

    add fetch logic

---
+++ reddit_pipeline/config.py
@@ -0,0 +1,3 @@
+REDDIT_CLIENT_ID = "GLj9ehqcVtdHFyJJEtpScQ"
+REDDIT_SECRET = "RQZYvAGTfU7avXOLKePeKb0jo-A0MA"
+REDDIT_USER_AGENT = "reddit-job-market-scraper by cc"
---
+++ reddit_pipeline/fetch.py
@@ -0,0 +1,17 @@
+import praw 
+from .config import REDDIT_CLIENT_ID, REDDIT_SECRET, REDDIT_USER_AGENT
+
+# api wrapper
+def init_reddit():
+    return praw.Reddit(
+        client_id= REDDIT_CLIENT_ID,
+        client_secret= REDDIT_SECRET,
+        user_agent= REDDIT_USER_AGENT,
+        )
+
+# fetch posts
+def fetch_posts(subreddit, keyword, limit=100):
+    reddit = init_reddit()
+    posts = reddit.subreddit(subreddit).search(keyword, limit=limit)
+    return posts
+
